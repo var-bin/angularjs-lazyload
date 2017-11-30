964351b688d69ec8a4e83aea9085363d82682414
var-bin <var-bin@users.noreply.github.com>
2017-11-30 16:38:15 +0200

    Delete useless configs

--- src/app.js
+++ src/app.js
@@ -6,15 +6,13 @@ import angular from "angular";
 import "@uirouter/angularjs";
 import "oclazyload";
 
-import { HOME_MODULE } from "./pages/home/home.module";
-import { appRouting } from "./app.routing";
+import "./pages/home/home.module";
 
 const LAZY_APP =  angular
   .module("lazyApp", [
     "ui.router",
     "oc.lazyLoad",
     "homeModule"
-  ])
-  .config(appRouting);
+  ]);
 
 export { LAZY_APP };
