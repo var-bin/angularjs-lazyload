733827a99b78f58caeeaf1ae2630b6bf56564c8e
var-bin <var-bin@users.noreply.github.com>
2017-12-11 17:42:38 +0200

    Using new routing config

--- src/app.js
+++ src/app.js
@@ -7,12 +7,14 @@ import "@uirouter/angularjs";
 import "oclazyload";
 
 import "./pages/home/home.module";
+import { appRouting } from "./app.routing";
 
 const LAZY_APP =  angular
   .module("lazyApp", [
     "ui.router",
     "oc.lazyLoad",
     "homeModule"
-  ]);
+  ])
+  .config(appRouting);
 
 export { LAZY_APP };
